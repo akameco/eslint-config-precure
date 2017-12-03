eb7272fd5d1910994843f71c1663d9ef371e6bf6
akameco <akameco.t@gmail.com>
2017-12-03 19:56:16 +0900

    feat(import): disalbe prefer default export

--- import/index.js
+++ import/index.js
@@ -61,7 +61,8 @@ module.exports = {
     'import/no-restricted-paths': 0,
     'import/no-unassigned-import': 2,
     'import/no-webpack-loader-syntax': 2,
-    'import/prefer-default-export': 1,
+    // export defaultはrollupできず、追いづらくなる可能性がある
+    'import/prefer-default-export': 0,
     'import/unambiguous': 0, // not sure I understand this rule well enough right now...
     'import/exports-last': 1,
   },
