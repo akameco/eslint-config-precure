50689925c74947a991fa2e575d40495534c5a7e8
akameco <akameco.t@gmail.com>
2019-02-06 10:01:05 +0900

    fix: disable react/jsx-filename-extension

--- react.js
+++ react.js
@@ -18,7 +18,9 @@ module.exports = {
     // 最適化すれば変わらないため無効。本質ではない。
     // 'react/prefer-stateless-function': 2,
     'react/jsx-boolean-value': 2,
-    'react/jsx-filename-extension': [2, { extensions: ['.js', '.tsx'] }],
+    // tsxが有効にならないので無効
+    'react/jsx-filename-extension': 0,
+    // 'react/jsx-filename-extension': [2, { extensions: ['.js', '.tsx'] }],
     'react/jsx-handler-names': 2,
     'react/prefer-es6-class': [2, 'always'],
   },
